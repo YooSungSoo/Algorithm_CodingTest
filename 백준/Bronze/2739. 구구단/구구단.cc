e6fd90de0e644338da58d2960eab1bd1bf225a40
Yoo Sung Soo <90916588+YooSungSoo@users.noreply.github.com>
2025-03-14 13:54:26 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a;
+
+int main(){
+
+	cin >> a;
+	
+	for(int i=1;i<10;i++){
+		cout << a << " * " << i << " = " << a*i << endl;
+	}
+	
+	return 0;
+}
