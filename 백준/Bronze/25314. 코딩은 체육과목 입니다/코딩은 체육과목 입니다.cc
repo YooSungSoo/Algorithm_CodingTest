d1c650ffc6950fb0bc0b03d000e6669420105903
Yoo Sung Soo <90916588+YooSungSoo@users.noreply.github.com>
2025-03-14 13:56:15 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a,b;
+
+int main(){
+
+	cin >> a;
+	b = a / 4;
+	for(int i=0;i<b;i++){
+		cout << "long ";
+	}
+	cout << "int";
+	
+	return 0;
+}
