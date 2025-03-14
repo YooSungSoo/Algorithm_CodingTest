a533dbd3736fee8e7eba8ec7137fefc994600ede
Yoo Sung Soo <90916588+YooSungSoo@users.noreply.github.com>
2025-03-14 13:49:11 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a,b;
+
+int main(){
+
+	cin >> a >> b;
+	if(a>b) cout << ">";
+	if(a<b) cout << "<";
+	if(a==b) cout << "==";
+	
+	return 0;
+}
