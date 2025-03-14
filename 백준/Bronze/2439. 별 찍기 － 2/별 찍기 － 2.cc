c467431b227575d3afb5d9d856f30d695afcc508
Yoo Sung Soo <90916588+YooSungSoo@users.noreply.github.com>
2025-03-14 13:58:34 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a;
+
+int main(){
+
+	cin >> a;
+	
+	for(int i=1;i<a+1;i++){
+		for(int k=a;k>i;k--){
+			cout << " ";
+		}
+		for(int j=0;j<i;j++){
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
