77d352a32795cda16891653a5b1284c73ae499c0
Yoo Sung Soo <90916588+YooSungSoo@users.noreply.github.com>
2025-03-26 16:59:09 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
+    cin >> n;
+
+    long long cnt = sqrt(n);
+    
+    cout << cnt << "\n";
+
+    return 0;
+}
