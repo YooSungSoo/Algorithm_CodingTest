fa703d08d505a666cf90a1c26e265f72d0868929
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-04-29 17:25:10 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 84 ms, Memory: 22844 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n,a,b,rst;
+int prefix[26][200001]; 
 string s;
-char c;
-
+int q;
 
 int main() {
-	
-	cin >> s >> n;
-	
-	while(n--){
-		char c;
-		rst = 0;
-		cin >> c >> a >> b;
-		
-		for(int i=a;i<b+1;i++){
-			if(s[i]==c) rst++;
-		}
-		cout << rst <<"\n";
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	return 0;
+    cin >> s >> q;
+
+    for (int i = 0; i < s.size(); i++) {
+        for (int j = 0; j < 26; j++) {
+            prefix[j][i + 1] = prefix[j][i];
+	    }
+        prefix[s[i] - 'a'][i + 1]++;
+    }
+
+    while (q--) {
+        char c;
+        int l, r;
+        cin >> c >> l >> r;
+
+        int idx = c - 'a';
+        cout << prefix[idx][r + 1] - prefix[idx][l] << "\n";
+    }
+
+    return 0;
 }
