0dd51e7d8e967271dd127673e4779f46422d4b70
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-05-02 19:44:43 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    for(int i=0;i<my_string.size();i++){
+        if(my_string[i] >= 'a' && my_string[i] <= 'z') continue; else if(my_string[i] >= 'A' && my_string[i] <= 'Z') continue; 
+        else answer += my_string[i] - '0';
+    }
+    return answer;
+}
