6c4fec366eeab0e74d3582edc54a28d041d141a2
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-05-06 19:58:15 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> numlist, int n) {
+    sort(numlist.begin(), numlist.end(), [n](int a, int b) {
+        int diffA = abs(a - n);
+        int diffB = abs(b - n);
+        if (diffA == diffB) return a > b; 
+        return diffA < diffB;             
+    });
+    return numlist;
+}
