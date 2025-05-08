22f88d69c978b4da58533c90b78f8da1ad6fca2d
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-05-08 18:17:39 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    string s = to_string(n);
+    sort(s.begin(),s.end(),greater<int>());
+    answer = stoll(s);
+    
+    return answer;
+}
