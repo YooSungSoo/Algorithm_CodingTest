34dfac1a8538da5f27e6aab570141c48ce146773
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-05-15 18:56:23 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.37 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int s : score) {
+        pq.push(s); 
+
+        if (pq.size() > k) {
+            pq.pop(); 
+        }
+
+        answer.push_back(pq.top()); 
+    }
+
+    return answer;
+}
