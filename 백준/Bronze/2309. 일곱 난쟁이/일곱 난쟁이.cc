7c3cceb4f91f349a4a944b65f28d37da7f6d55c7
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-07-15 22:42:20 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,38 +1,31 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int n,a[9],sum,p,x,y;
-
+int n,a,b;
 int main() {
-	
+    
+    vector<int> v;
+    int rst = 0;
 	for(int i=0;i<9;i++){
 		cin >> n;
-		a[i] = n;
-		sum +=n;
+		v.push_back(n);
+		rst+=n;
 	}
 	
-	for(int i=0;i<9;i++){
+	for(int i=0;i<8;i++){
 		for(int j=i+1;j<9;j++){
-			p = a[i] + a[j];
-			if(sum - p==100){
-				x=i;
-				y=j;
+			if(rst - v[i] - v[j]==100){
+				a = v[i];
+				b = v[j];
 			}
 		}
 	}
-	vector<int> s;
-	
+	vector<int> v1;
 	for(int i=0;i<9;i++){
-		if(i==y) continue;
-		else if(i==x) continue;
-		else  s.push_back(a[i]);
+		if(v[i]==a || v[i]==b) continue;
+		else v1.push_back(v[i]);
 	}
-	sort(s.begin(),s.end());
-	for(int i=0;i<7;i++){
-		cout << s[i] << endl;
+	sort(v1.begin(),v1.end());
+	for(auto i : v1){
+		cout << i << "\n";
 	}
-	
-
-    return 0;
 }
