020d68306cb4c18aeda03a407597af397c704a83
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-07-16 21:55:11 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,27 +1,19 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 string s;
 
 int main() {
-	
+ 
 	getline(cin,s);
 	
-	for(int i=0;i<s.length();i++){
-		
-		if(s[i]>='A' && s[i]<='Z'){
-			if(s[i] + 13 >'Z') s[i] = s[i] - 26 + 13;
-			else s[i] = s[i] + 13;	
-			
-			}
-		else if(s[i]>='a' && s[i]<='z'){
-			if(s[i] + 13 >'z') s[i] = s[i] - 26 + 13;
-			else s[i] = s[i] + 13;	
-			
-			}
+	for(int i=0;i<s.size();i++){
+		if(isupper(s[i])){
+			s[i] = (s[i] - 'A' + 13) % 26 + 'A';
+		}else if(islower(s[i])){
+			s[i] = (s[i] - 'a' + 13) % 26 + 'a';
+		}
 	}
-
 	cout << s;
+	
 	return 0;
 }
