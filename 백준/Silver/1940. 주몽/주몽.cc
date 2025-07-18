8a801cf61ae29d664f260b263a2c4dfd6743752f
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-07-18 14:58:18 +0900

    [Silver IV] Title: 주몽, Time: 112 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,22 +1,21 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
+using namespace std;
 
-using namespace std; 
+int n,k;
+int cnt;
+int main(){
 
-int n,m,cnt;
-
-int main() {
-
-	cin >> n >> m;
-	
-	vector<int> s(n);
-	
+	cin >> n >> k;
+	vector<int> v;
 	for(int i=0;i<n;i++){
-		cin >> s[i];
+		int a = 0;
+		cin >> a;
+		v.push_back(a);
 	}
 	
 	for(int i=0;i<n;i++){
 		for(int j=i+1;j<n;j++){
-			if(s[i] + s[j]==m) cnt++;
+			if(v[i] + v[j]==k) cnt++;
 		}
 	}
 	
