8ab40b9e40d54abb4574ae764d5d721413947edb
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-07-24 15:43:52 +0900

    [Silver V] Title: 숫자 카드, Time: 192 ms, Memory: 8040 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -4,23 +4,38 @@ using namespace std;
 int n, k;
 int a, b;
 
+bool cal(vector<int> &arr, int target){
+	int left = 0, right = arr.size() - 1;
+	
+	while(left<=right){
+		int mid = (left + right) / 2;
+		if(target == arr[mid]) return true;
+		else if(arr[mid] < target) left = mid + 1;
+		else right = mid - 1;
+	}
+	return false;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
 	cin >> n;
-	set<int> s;
+	vector<int> v;
 	
 	for(int i=0;i<n;i++){
 		cin >> k;
-		s.insert(k);
+		v.push_back(k);
 	}
+	sort(v.begin(),v.end());
+	
 	cin >> a;
-	for(int i=0;i<a;i++){
+	string result;
+	while(a--){
 		cin >> b;
-		if(s.count(b)==1) cout << 1 << " ";
-		else cout << 0 << " ";
+		result += (cal(v,b) ? "1 " : "0 ");
 	}
-
+	
+	cout << result << "\n";
     return 0;
 }
