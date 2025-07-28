fd80aa4cb4bc689f670bf8ed26fd7641f5703d96
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-07-28 16:02:58 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,50 +1,48 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
+int n;
+char p, l, r;
 int tree[26][2];
 
-
-void preorder(int node) {
-    cout << char(node + 'A');
-    if (tree[node][0] != -1) preorder(tree[node][0]); 
-    if (tree[node][1] != -1) preorder(tree[node][1]);
+void preOrder(int node){
+	cout << char(node + 'A');
+	if(tree[node][0]!=-1) preOrder(tree[node][0]);
+	if(tree[node][1]!=-1) preOrder(tree[node][1]);
+}
+void inOrder(int node){
+	if(tree[node][0]!=-1) inOrder(tree[node][0]);
+	cout << char(node + 'A');
+	if(tree[node][1]!=-1) inOrder(tree[node][1]);
+}
+void postOrder(int node){
+	if(tree[node][0]!=-1) postOrder(tree[node][0]);
+	if(tree[node][1]!=-1) postOrder(tree[node][1]);
+	cout << char(node + 'A');
 }
 
-void inorder(int node) {
-    if (tree[node][0] != -1) inorder(tree[node][0]); 
-    cout << char(node + 'A'); 
-    if (tree[node][1] != -1) inorder(tree[node][1]);
-}
 
-void postorder(int node) {
-    if (tree[node][0] != -1) postorder(tree[node][0]);
-    if (tree[node][1] != -1) postorder(tree[node][1]);
-    cout << char(node + 'A');
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < 26; i++) {
-        tree[i][0] = tree[i][1] = -1;
-    }
-
-    for (int i = 0; i < n; i++) {
-        char parent, left, right;
-        cin >> parent >> left >> right;
-
-        int p = parent - 'A'; 
-        if (left != '.') tree[p][0] = left - 'A';  
-        if (right != '.') tree[p][1] = right - 'A';
-    }
-
-    preorder(0); cout << "\n";
-    inorder(0); cout << "\n";
-    postorder(0); cout << "\n";
-
-    return 0;
+int main(void){
+	
+	cin >> n;
+	
+	for(int i=0;i<26;i++){
+		tree[i][0] = tree[i][1] = -1;
+	}
+	
+	for(int i=0;i<n;i++){
+		cin >> p >> l >> r;
+		if(l != '.') tree[p-'A'][0] = l-'A';
+		if(r != '.') tree[p-'A'][1] = r-'A';
+	}
+	
+	preOrder(0);
+	cout << "\n";
+	inOrder(0);
+	cout << "\n";
+	postOrder(0);
+	cout << "\n";
+	
+	return 0;
 }
