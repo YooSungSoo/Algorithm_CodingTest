863dcdf288e53b79b0c0013307f4c188b2e8f876
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-08-04 11:32:04 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+const int MOD = 15746;
+int dp[1000001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    }
+
+    cout << dp[n] << '\n';
+    return 0;
+}
