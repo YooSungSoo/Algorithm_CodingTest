e507843c01b1b6205de11fea33344fc5d344a5e5
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-08-15 11:33:31 +0900

    [Silver V] Title: 배열 합치기, Time: 540 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -10,17 +10,14 @@ int main() {
 	cin >> n >> m;
 	
 	vector<int> v;
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n+m;i++){
 		int a = 0;
 		cin >> a;
 		v.push_back(a);
 	}
-	for(int i=0;i<m;i++){
-		int b = 0;
-		cin >> b;
-		v.push_back(b);
-	}
+
 	sort(v.begin(),v.end());
+	
 	for(auto k : v){
 		cout << k << " ";
 	}
