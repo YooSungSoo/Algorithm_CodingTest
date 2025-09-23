d210472cdd859f9a8e517f3bd7a480082dc000f3
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-09-23 15:29:42 +0900

    [level 2] Title: 구명보트, Time: 1.98 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    sort(people.begin(), people.end());
+    int left = 0, right = people.size() - 1;
+    int answer = 0;
+    
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++; 
+        }
+        right--; 
+        answer++;
+    }
+    
+    return answer;
+}
