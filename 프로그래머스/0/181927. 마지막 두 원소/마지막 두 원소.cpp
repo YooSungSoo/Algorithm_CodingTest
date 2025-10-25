d69869fa1f88d879950277330e928550cbe7b3af
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-10-25 17:40:54 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    int n = num_list.size();
+    if(num_list[n-2] < num_list[n-1])
+        num_list.push_back(num_list[n-1]-num_list[n-2]);
+    else num_list.push_back(num_list[n-1] * 2);
+    return num_list;
+}
